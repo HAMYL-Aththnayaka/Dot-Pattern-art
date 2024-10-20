1118352abbd5a7c98e4c55a2c2b6b08efaf4c1a0
Yasas Aththanayaka <Aththanayakayasas@gmail.com>
2024-10-21 01:28:34 +0530

    Add files via upload

---
+++ Dot pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i=1;i<6;i++)
+    {
+        for (int j=9; j>0; j--)
+            {
+            cout<<"*";
+            }
+            cout<<endl;
+        for (int j=5;j>0;j--)
+        {
+            cout<<"* ";
+        }
+    cout<<endl;
+    }
+return 0;
+}
